edcf3fac51a92cd8e522e02f40a3ebb5f238b34f
Matias <s2101032@edu.bc.fi>
2023-11-21 11:38:24 +0200

    menuja lisää

---
+++ juoksupeli/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MenuManager
+{
+    public static GameObject mainMenu, settingsMenu, shopMenu;
+    public static void Init()
+    {
+        GameObject canvas = GameObject.Find("Canvas");
+    }
+}
