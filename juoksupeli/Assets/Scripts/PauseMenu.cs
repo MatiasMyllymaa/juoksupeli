165fe1eb07f839e42051ab73c2e89350f9d2b3bc
Matias <s2101032@edu.bc.fi>
2023-11-22 12:40:28 +0200

    esc avaa pausemenun

--- juoksupeli/Assets/Scripts/PauseMenu.cs
+++ juoksupeli/Assets/Scripts/PauseMenu.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject pauseMenuUI; // Reference to the pause menu UI in the scene
+    private bool isPaused = false; // Flag to track whether the game is currently paused
+
     void Start()
     {
-        
+        // Make sure the pause menu is initially hidden
+        pauseMenuUI.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Check if the Escape key is pressed
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // Toggle the pause state
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    void PauseGame()
+    {
+        // Activate the pause menu UI
+        pauseMenuUI.SetActive(true);
+
+        // Pause the game
+        Time.timeScale = 0f;
+
+        // Set the paused flag to true
+        isPaused = true;
+    }
+
+    void ResumeGame()
+    {
+        // Deactivate the pause menu UI
+        pauseMenuUI.SetActive(false);
+
+        // Resume the game
+        Time.timeScale = 1f;
+
+        // Set the paused flag to false
+        isPaused = false;
     }
 }
