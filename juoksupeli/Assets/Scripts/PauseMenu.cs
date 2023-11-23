ff3483f86fdcb6b436f7d350797be6fa01f827fc
Matias <s2101032@edu.bc.fi>
2023-11-23 12:50:32 +0200

    roughtimer

--- juoksupeli/Assets/Scripts/PauseMenu.cs
+++ juoksupeli/Assets/Scripts/PauseMenu.cs
@@ -1,24 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    public GameObject pauseMenuUI; // Reference to the pause menu UI in the scene
-    private bool isPaused = false; // Flag to track whether the game is currently paused
+    public GameObject pauseMenuUI;
+    private bool isPaused = false;
 
     void Start()
     {
-        // Make sure the pause menu is initially hidden
         pauseMenuUI.SetActive(false);
     }
 
     void Update()
     {
-        // Check if the Escape key is pressed
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            // Toggle the pause state
             if (isPaused)
             {
                 ResumeGame();
@@ -32,25 +30,29 @@ public class PauseMenu : MonoBehaviour
 
     void PauseGame()
     {
-        // Activate the pause menu UI
         pauseMenuUI.SetActive(true);
 
-        // Pause the game
         Time.timeScale = 0f;
 
-        // Set the paused flag to true
         isPaused = true;
     }
 
     void ResumeGame()
     {
-        // Deactivate the pause menu UI
         pauseMenuUI.SetActive(false);
 
-        // Resume the game
         Time.timeScale = 1f;
 
-        // Set the paused flag to false
         isPaused = false;
+    }
+
+    public void OnResumeButtonClick()
+    {
+        ResumeGame();
+    }
+
+    public void OnMainMenuButtonClick()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
